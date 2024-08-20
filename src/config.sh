eb0e3b929a97b53ad9a237176bee46dff5199079
epadgaia <epadgaia@sv-epg00.epadgaia.local>
2024-08-20 19:27:23 +0100

    update:large update to all scripts

--- src/config.sh
+++ src/config.sh
@@ -2,6 +2,12 @@
 
 x="start"
 
+LOGFILE="./logs/config.log"
+
+log_message() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> $LOGFILE
+}
+
 menu ()
 {
 while true $x != "start"
@@ -16,6 +22,18 @@ echo "    Update Server               - 1"
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "    Reboot Server               - 2"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "    Configure Static IP         - 3"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "    Configure DNS (resolv.conf) - 4"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "    View Network Config         - 5"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "    Set Hostname and Domain     - 6"
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "    Return                      - 9"
@@ -34,58 +52,144 @@ echo "==========================================================================
 
 case "$x" in
 
+  1)
+    echo ""
+    echo "Update all Server packages..."
+    echo ""
+    sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt clean
+    echo ""
+    echo "SUCCESS!"
+    log_message "Updated server packages"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
+;;
+  2)
+    echo ""
+    echo "Rebooting..."
+    log_message "Server reboot initiated"
+    echo ""
+    sudo reboot
+;;
+  3) 
+    bkpdate=$(date +-'%F'-'%s')
+    sudo cp /etc/netplan/99-custom.yaml /etc/netplan/99-custom.yaml$bkpdate.bkp
 
-    1)
-      echo ""
-      echo "Update all Server packages..."
-      echo ""
-      sudo apt update
-      sudo apt upgrade -y
-      sudo apt dist-upgrade -y
-      sudo apt autoremove -y
-      sudo apt clean
-      echo ""
-echo ""
-echo 
-"=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    2)
-      echo ""
-      echo "Rebooting..."
-      echo ""
-      sudo reboot
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    9)
-      echo ""
-      echo "Returning..."
-      echo ""
-      src/server.sh
-      exit
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    0)
-      echo ""
-      echo "Exiting..."
-      echo ""
-      clear
-      exit
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
+    echo ""
+    echo "Configuring Static IP..."
+    ip link show | awk -F': ' '{print $2}' | grep -v '^lo$'
+    echo "Enter the network interface (e.g., enp2s0): "
+    read iface
+    echo "Enter the static IP address (e.g., 192.168.1.181/24): "
+    read ipaddr
+    echo "Enter the gateway (e.g., 192.168.1.1): "
+    read gateway
+    echo "Enter the primary DNS server (e.g., 8.8.8.8): "
+    read dns1
+    echo "Enter the secondary DNS server (optional, e.g., 1.1.1.1): "
+    read dns2
+    echo "Enter the tertiary DNS server (optional, e.g., 9.9.9.9): "
+    read dns3
 
-*)
-        echo "Invalid option!"
+    sudo bash -c "echo '
+    network:
+        version: 2
+        ethernets:
+            $iface:
+                addresses:
+                    - $ipaddr
+                nameservers:
+                    addresses:
+                        - $dns1
+                        ${dns2:+- $dns2}
+                        ${dns3:+- $dns3}
+                routes:
+                    - to: default
+                      via: $gateway
+    ' > /etc/netplan/99-custom.yaml" -- "$iface" "$ipaddr" "$dns1" "$dns2" "$dns3" "$gateway"
+    sudo netplan apply
+    echo "Static IP and DNS configured!"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
+;;
+  4)
+    bkpdate=$(date +-'%F'-'%s')
+    sudo cp /etc/resolv.conf /etc/resolv.conf$bkpdate.bkp
+    echo ""
+    echo "Configuring DNS (resolv.conf)..."
+    echo "Enter the DNS server addresses (comma-separated, e.g., 8.8.8.8,8.8.4.4): "
+    read dns
+
+    sudo bash -c "echo -e 'nameserver $(echo $dns | sed 's/,/\n/g')' > /etc/resolv.conf"
+
+    echo "resolv.conf configured!"
+    log_message "Configured resolv.conf with DNS: $dns"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
+;;
+  5)
+    echo ""
+    echo "Current Network Configuration:"
+    ip a
+    echo ""
+    echo "Press <ENTER> to continue..."
+    log_message "Viewed network configuration"
+    read p
+;;
+  6)
+    bkpdate=$(date +-'%F'-'%s')
+    sudo cp /etc/hosts /etc/hosts$bkpdate.bkp
+    echo ""
+    echo "Setting Hostname and Domain..."
+    echo ""
+    echo "Acctual Hostname and FQDN:"
+    echo ""
+    hostname -f
+    echo ""
+    echo "Enter the new hostname: "
+    read newhostname
+    echo ""
+    echo "Enter the FQDN (Fully Qualified Domain Name) for this host, if any (e.g., example.local):"
+    read fqdn
+    echo ""
+    sudo hostnamectl set-hostname "$newhostname"
+    echo "Updating /etc/hosts..."
+    sudo sed -i "s/127.0.1.1.*/127.0.1.1 $newhostname/" /etc/hosts
+    echo "Updated /etc/hosts with new hostname."
+    if [ -n "$fqdn" ]; then
+      echo "Updating /etc/hosts with FQDN..."
+      sudo sed -i "/$newhostname/d" /etc/hosts
+      echo "127.0.1.1 $newhostname"."$fqdn $newhostname" | sudo tee -a /etc/hosts > /dev/null
+      echo "Updated /etc/hosts with FQDN."
+    else
+      echo "No FQDN provided. Only hostname is set."
+    fi
+    echo "Hostname set to $newhostname!"
+    echo "Domain set to $newdomain!"
+    log_message "Hostname and /etc/hosts set to $newhostname"
+    log_message "Domain on /etc/hosts set to $newdomain"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
+;;
+  9)
+    echo ""
+    echo "Returning..."
+    echo ""
+    src/server.sh
+    exit
+;;
+  0)
+    echo ""
+    echo "Exiting..."
+    echo ""
+    clear
+    exit
+;;
+  *)
+    echo "Invalid option!"
 esac
 done
 
--- src/samba.sh
+++ src/samba.sh
@@ -2,6 +2,12 @@
 
 x="start"
 
+LOGFILE="./logs/samba.log"
+
+log_message() {
+  echo "$(date +'%Y-%m-%d %H:%M:%S') - $1" >> $LOGFILE
+}
+
 menu ()
 {
 while true $x != "start"
@@ -12,22 +18,25 @@ echo "                                   SAMBA"
 echo "=============================================================================================="
 echo ""
 echo "------------------------------------------"
-echo "    Install / Update SAMBA      - 1"
+echo "    Install SAMBA Services      - 1"
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "    Add User to SAMBA           - 2"
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "    Create SAMBA Server         - 3"
+echo "    Configure SAMBA-AD-DC       - 3"
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "    List SAMBA Users            - 4"
+echo "    Configure Kerberos          - 4"
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "    Status SAMBA Server         - 5"
+echo "    List SAMBA Users            - 5"
 echo "------------------------------------------"
 echo "------------------------------------------"
-echo "    Restart SAMBA Server        - 6"
+echo "    Status SAMBA Server         - 6"
+echo "------------------------------------------"
+echo "------------------------------------------"
+echo "    Restart SAMBA Server        - 7"
 echo "------------------------------------------"
 echo "------------------------------------------"
 echo "    Return                      - 9"
@@ -46,140 +55,250 @@ echo "==========================================================================
 
 case "$x" in
 
+  1)
+    echo ""
+    echo "Install all SAMBA Services and Packages..."
+    echo ""
+    sudo apt install libcups2 samba samba-common samba-dsdb-modules samba-vfs-modules cups krb5-user winbind openssl -y
+    echo ""
+    echo "SUCCESS!"
+    log_message "Installed SAMBA Services and Packages"
+    echo ""
+    echo "Press <ENTER> for continue..."
+    read p
+;;
+  2)
+    echo ""
+    echo "Enter the USER you want to add to SAMBA: "
+    read sambauser
+    echo ""
+    sudo smbpasswd -a $sambauser
+    echo ""
+    log_message "Added user $sambauser to Samba"
+    echo ""
+    echo "Press <ENTER> for continue..."
+    read p
+;;
+  3)
+    echo ""
+    echo "Configuring Samba AD DC..."
+    bkpdate=$(date +-'%F'-'%s')
+    sudo cp /etc/samba/smb.conf /etc/samba/smb.conf$bkpdate.bkp
 
-    1)
-      echo ""
-      echo "Install / Update all SAMBA packages..."
-      echo ""
-      sudo apt install libcups2 samba samba-common cups -y
-      echo ""
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
+    sudo systemctl enable winbind
+    sudo systemctl start winbind
+    sudo systemctl restart smbd nmbd winbind samba-ad-dc
+    sudo useradd -r -g samba -s /sbin/nologin samba
+    sudo groupadd samba
+    sudo chown -R samba:samba /var/lib/samba/sysvol
+    sudo chmod 755 /var/lib/samba/sysvol
+
+    hostname=$(hostname)
+    fqdn=$(hostname -f)
+    nameserver=$hostname
+    realm=$(echo "$fqdn" | awk -F'.' '{print toupper($2"."$3)}')
+    workgroup=$(echo "$realm" | awk -F'.' '{print $1}')
+
+    echo "Check if SAMBA TLS certificates already exist..."
+    if [ -f /etc/samba/tls/private_key.pem ] && [ -f /etc/samba/tls/certificate.crt ]; then
+        echo "TLS certificates already exist."
+        echo "Do you want to regenerate the certificates? (y/n): "
+        read regenerate
+        if [ "$regenerate" != "y" ]; then
+            echo "Skipping certificate generation."
+            regenerate_certificates=false
+        else
+            regenerate_certificates=true
+        fi
+    else
+        regenerate_certificates=true
+    fi
+
+    if [ "$regenerate_certificates" = true ]; then
+      echo "Generating SAMBA TLS certificates..."
+      sudo bash -c "echo '
+      [ req ]
+      default_bits       = 2048
+      distinguished_name = req_distinguished_name
+      x509_extensions    = v3_ca
+      req_extensions     = v3_req
+
+      [ req_distinguished_name ]
+      countryName            = Country Name (2 letter code)
+      countryName_default    = US
+      stateOrProvinceName    = State or Province Name (full name)
+      stateOrProvinceName_default = State
+      localityName           = Locality Name (e.g., city)
+      localityName_default   = City
+      organizationalUnitName = Organizational Unit Name (e.g., section)
+      organizationalUnitName_default = IT
+      commonName             = Common Name (e.g., server FQDN or YOUR name)
+      commonName_default     = $fqdn
+      commonName_max         = 64
+
+      [ v3_ca ]
+      subjectKeyIdentifier   = hash
+      authorityKeyIdentifier = keyid:always,issuer:always
+      basicConstraints       = CA:TRUE
+      keyUsage                = keyCertSign, cRLSign
+
+      [ v3_req ]
+      basicConstraints       = CA:FALSE
+      keyUsage                = digitalSignature, keyEncipherment
+      ' > /tmp/openssl.cnf"
+
+      sudo openssl genrsa -out /etc/samba/tls/private_key.pem 2048
+      sudo openssl req -x509 -new -nodes -key /etc/samba/tls/private_key.pem -sha256 -days 365 -out /etc/samba/tls/certificate.crt -config /tmp/openssl.cnf
+    echo "TLS certificates generated successfully."
+    else
+      echo "Continuing with existing TLS certificates."
+    fi
+
+    echo "Apply SAMBA settings..."
+    sudo bash -c "echo '
+    [global]
+      netbios name = $nameserver
+      realm = $realm
+      workgroup = $workgroup
+      server role = active directory domain controller
+      idmap_ldb:use rfc2307 = yes
+      log file = /var/log/samba/log.%m
+      max log size = 1000
+      panic action = /usr/share/samba/panic-action %d
+      server string = $nameserver server (Samba, Ubuntu)
+      log level = 3
+      idmap config *: range = 1000000-1999999
+      idmap config *: backend = tdb
+      tls enabled = yes
+      tls keyfile = /etc/samba/tls/private_key.pem
+      tls certfile = /etc/samba/tls/certificate.crt
+
+    [netlogon]
+      path = /var/lib/samba/sysvol/$realm/scripts
+      read only = no
+
+    [sysvol]
+      path = /var/lib/samba/sysvol
+      read only = no
+
+    [$nameserver]
+      path = /var/lib/samba/sysvol
+      read only = no
+    ' > /etc/samba/smb.conf"
+    echo "Generate SAMBA Domain..."
+    echo ""
+    echo "Enter the Domain Password: "
+    read -s password
+    sudo samba-tool domain provision \
+    --use-rfc2307 \
+    --server-role=dc \
+    --dns-backend=SAMBA_INTERNAL \
+    --domain=$workgroup \
+    --realm=$realm \
+    --adminpass=$password
+    sudo systemctl restart smbd nmbd winbind samba-ad-dc
+    testparm
+    echo ""
+    echo "SUCCESS! Samba AD DC configuration has been applied."
+    echo ""
+    log_message "Configured Samba AD DC with server name $nameserver and realm $realm"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
 ;;
-    2)
-      echo ""
-      echo "Enter the USER you want to add to SAMBA: "
-      read sambauser
-      echo ""
-      sudo smbpasswd -a $sambauser
-      echo ""
-      echo "SUCCESS!"
-      sleep 1
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
+  4)
+    bkpdate=$(date +-'%F'-'%s')
+    sudo cp /etc/krb5.conf /etc/krb5.conf$bkpdate.bkp
+    
+    echo ""
+    echo "Configuring Kerberos..."
+    fqdn=$(hostname -f)
+    realm=$(echo "$fqdn" | awk -F'.' '{print toupper($2"."$3)}')
+    echo ""
+    sudo bash -c "echo '
+    [libdefaults]
+    default_realm = $realm
+    dns_lookup_realm = false
+    dns_lookup_kdc = true
+
+    [realms]
+      $realm = {
+        kdc = kerberos.$realm
+        admin_server = kerberos.$realm
+      }
+
+    [domain_realm]
+      .$realm = $realm
+      $realm = $realm
+    ' > /etc/krb5.conf" -- "$realm"
+
+    echo "Kerberos configuration file created."
+    echo ""
+    log_message "Configured Kerberos for domain $realm"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
 ;;
-    3)
-      bkpdate=$(date +-'%F'-'%s')
-      sudo cp /etc/samba/smb.conf /etc/samba/smb.conf$bkpdate.bkp 
-      echo ""
-      echo "Enter the SAMBA server NAME: "
-      read nameserver
-      echo ""
-      echo "Enter a comment (or leave blank) for the SAMBA server: "
-      read comment
-      echo ""
-      echo "Enter the directory for the SAMBA server, Ex.:(/home/server): "
-      read path
-      echo ""
-      echo "Inform if the directory can be BROWSABLE: [yes] or [no]"
-      read browsable
-      echo ""
-      echo "Inform if the file can be READ ONLY: [yes] or [no]"
-      read readon
-      echo ""
-      echo "Inform if the server allows GUEST: [yes] or [no]"
-      read guest
-      echo ""
-      echo "Enter the USER / GROUP that has permission to access this server: "
-      read valid
-      echo ""
-      sudo echo "
-        [$nameserver]
-        comment = $comment
-        path = $path
-        browseable = $browsable
-        read only = $readon
-        guest ok = $guest
-        valid users = @$valid
-      " >> /etc/samba/smb.conf
-      echo ""
-      echo "SUCCESS!"
-      sleep 1
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    4)
-      echo ""
+  5)
+    echo ""
+    echo "Enter [C]omplete or [S]imple Info [Default: (S)]: "
+    read option
+    echo ""
+
+    if [[ "$option" == "C" || "$option" == "c" ]]; then
+      echo "Listing complete user info..."
       sudo pdbedit -L -v
-      echo ""
-      echo "SUCCESS!"
-      echo ""
-      echo "Press <ENTER> for continue..."
-      read p
-echo ""
-echo 
-"=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    5)
-      echo ""
-      sudo /etc/init.d/smbd status
-      echo ""
-      echo "SUCCESS!"
-      echo ""
-      echo "Press <ENTER> for continue..."
-      read p
-echo ""
-echo 
-"=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    6)
-      echo ""
-      sudo /etc/init.d/smbd restart
-      echo ""
-      echo "SUCCESS!"
-      echo ""
-      sleep 1
-echo ""
-echo 
-"=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    9)
-      echo ""
-      echo "Returning..."
-      echo ""
-      src/server.sh
-      exit
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
-    0)
-      echo ""
-      echo "Exiting..."
-      echo ""
-      clear
-      exit
-echo ""
-echo "=============================================================================================="
-echo ""
-echo "=============================================================================================="
-;;
+    else
+      echo "Listing simple user info..."
+      sudo samba-tool user list 
+    fi
 
-*)
-        echo "Invalid option!"
+    echo ""
+    echo "SUCCESS!"
+    echo ""
+    log_message "Listed Samba users"
+    echo ""
+    echo "Press <ENTER> to continue..."
+    read p
+;;
+  6)
+    echo ""
+    sudo /etc/init.d/samba-ad-dc status
+    echo ""
+    echo "SUCCESS!"
+    echo ""
+    log_message "Checked Samba server status"
+    echo ""
+    echo "Press <ENTER> for continue..."
+    read p
+;;
+  7)
+    echo ""
+    sudo /etc/init.d/smbd restart
+    sudo systemctl restart smbd nmbd winbind samba-ad-dc
+    echo ""
+    echo "SUCCESS!"
+    echo ""
+    log_message "Restarted Samba server"
+    echo ""
+    sleep 1
+;;
+  9)
+    echo ""
+    echo "Returning..."
+    echo ""
+    src/server.sh
+    exit
+;;
+  0)
+    echo ""
+    echo "Exiting..."
+    echo ""
+    clear
+    exit
+;;
+  *)
+    echo "Invalid option!"
 esac
 done
 
